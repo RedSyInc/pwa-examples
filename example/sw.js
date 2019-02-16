7a3291c685b56c3e7bd14056261ed074ecdf01be
«Oleynikov» <oleynikov@real.me>
2019-02-16 18:43:10 +0200

    start

--- example/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-
